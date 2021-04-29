8bf010a613bed18f0703dcf24fdbd187daae0c25
Lejla Hećo <hecolejla@hotmail.com>
2021-04-29 18:34:28 +0200

    Playerprefs saving collected coins score

--- Fantasy Town Joyride/Assets/Scripts/CoinManager.cs
+++ Fantasy Town Joyride/Assets/Scripts/CoinManager.cs
@@ -1,21 +1,24 @@
 using System.Collections;
+using Spacecraft.Controllers.Core.Entities;
 using Spacecraft.Controllers.Core.LevelGenerator;
 using UnityEngine;
 
 namespace Spacecraft
 {
-public class CoinManager : MonoBehaviour
+public class CoinManager : TrackedEntity
 {
     private int RotateSpeed;
     private int Delay;
-    private static int Points = 0;
+    private static int Points;
     private void Start()
     {
+        Points = PlayerPrefs.GetInt("CollectedMoney",0);
         RotateSpeed = 3;
         Delay = 2;
     }
     private void Update()
     {
+        if (IsPaused) PlayerPrefs.SetInt("CollectedMoney", Points);
         transform.Rotate(0, RotateSpeed, 0, Space.World);
     }
     
@@ -31,6 +34,8 @@ public class CoinManager : MonoBehaviour
                 Deactivate();
                 Points += 1;
         }
+
+        Debug.Log("CollectedCoins = " + Points);
     }
 
     private void Deactivate()
