a8cee8929cd4b18e3e3b1be75dc5e7cd71b2480e
Lejla Hećo <hecolejla@hotmail.com>
2021-04-30 14:28:35 +0200

    Coin pick up sound added

--- Fantasy Town Joyride/Assets/Scripts/CoinCollector.cs
+++ Fantasy Town Joyride/Assets/Scripts/CoinCollector.cs
@@ -11,7 +11,7 @@ namespace Spacecraft
         private int Delay;
         private int Points;
         private int ThisGamesPoints = 0;
-
+        [SerializeField] private AudioClip CoinPickUpSound;
         private void Start()
         {
             Points = PlayerPrefs.GetInt("CollectedMoney", 0);
@@ -27,13 +27,16 @@ namespace Spacecraft
         {
             if (other.gameObject.CompareTag("BigGem"))
             {
+                PlayCoinPickUpSound(other);
                 other.gameObject.SetActive(false);
                 ThisGamesPoints += 2;
                 Reactivate(other);
+                
             }
 
             if (other.gameObject.CompareTag("SmallGem"))
             {
+                PlayCoinPickUpSound(other);
                 other.gameObject.SetActive(false);
                 ThisGamesPoints += 1;
                 Reactivate(other);
@@ -47,6 +50,11 @@ namespace Spacecraft
         {
             yield return new WaitForSeconds(Delay);
             other.gameObject.SetActive(true);
+        }
+
+        private void PlayCoinPickUpSound(Collider other)
+        {
+            AudioSource.PlayClipAtPoint(CoinPickUpSound, other.gameObject.transform.position);
         }
     }
 }
