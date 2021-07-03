68ae64514d23b9652f70d547952b94430b6b2272
Lejla Hećo <hecolejla@hotmail.com>
2021-07-03 10:31:38 +0200

    UI design improved

--- Fantasy Town Joyride/Assets/Scripts/UI/MainMenu.cs
+++ Fantasy Town Joyride/Assets/Scripts/UI/MainMenu.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
 
 namespace Spacecraft.UI
 {
     public class MainMenu : MonoBehaviour
     {
+        [SerializeField] private RawImage load1;
+        [SerializeField] private RawImage load2;
+        [SerializeField] private RawImage load3;
+        [SerializeField] private RawImage load4;
+        [SerializeField] private RawImage load5;
+        [SerializeField] private RawImage load6;
         public void PlayGame()
         {
-            // SceneManager.LoadScene("Scenes/GameScene");
             StartCoroutine(AsynchronousLoad("Scenes/GameScene"));
         }
 
@@ -30,19 +37,43 @@ namespace Spacecraft.UI
             ao.allowSceneActivation = false;
             while (!ao.isDone)
             {
-                // [0, 0.9] > [0, 1]
                 float progress = Mathf.Clamp01(ao.progress / 0.9f);
-                Debug.Log("Loading progress: " + (progress * 100) + "%");
+                CheckProgressValue(progress);
                 // Loading completed
                 if (ao.progress == 0.9f)
                 {
-                    Debug.Log("Press a key to start");
-                    // if (Input.anyKey)
+                    load5.gameObject.SetActive(false);
+                    load6.gameObject.SetActive(true);
                     ao.allowSceneActivation = true;
                 }
 
                 yield return null;
             }
         }
+
+        private void CheckProgressValue(float progress)
+        {
+            if (progress >= 0.2f && progress < 0.4f)
+            {
+                load1.gameObject.SetActive(false);
+                load2.gameObject.SetActive(true);
+            }
+            else if (progress >= 0.4f && progress < 0.6f)
+            {
+                load2.gameObject.SetActive(false);
+                load3.gameObject.SetActive(true);
+            }
+            else if (progress >= 0.6f && progress < 0.8f)
+            {
+                load3.gameObject.SetActive(false);
+                load4.gameObject.SetActive(true);
+            }
+            else if (progress >= 0.8f && progress < 0.9f)
+            {
+                load4.gameObject.SetActive(false);
+                load5.gameObject.SetActive(true);
+            }
+        }
+        
     }
 }
